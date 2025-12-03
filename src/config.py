7f5513ed6adb4739cd592615e6418e7c1f5009c7
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2025-12-03 11:24:14 +0300

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,6 @@
+# Res2Net configuration
+NUM_CLASSES = 1000       # ImageNet default
+INPUT_SIZE = (3, 224, 224)
+LAYERS = [3, 4, 6, 3]    # ResNet-50 style
+SCALE = 4                # Res2Net scale s
+IN_CHANNELS = 64         # Stem channels
