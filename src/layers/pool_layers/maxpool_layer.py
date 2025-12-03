61277a9b8dbd249278eaaa99f0b71230a8aaa968
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2025-12-03 11:18:01 +0300

    Create maxpool_layer.py

---
+++ src/layers/pool_layers/maxpool_layer.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+
+class MaxPoolLayer(nn.Module):
+    def __init__(self, kernel_size=2, stride=2, padding=0):
+        super().__init__()
+        self.pool = nn.MaxPool2d(kernel_size, stride, padding)
+    
+    def forward(self, x):
+        return self.pool(x)
